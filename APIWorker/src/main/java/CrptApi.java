ecfa653e88666ece6995f38384aff733a83bfcfc
pavelmalkov <pashamalkovich@mail.ru>
2024-01-30 04:30:55 +0300

    Stable version
    - Реализована логика отправки документа.
    - Реализована очередь отправки документов, превысивших лимит(такие документы отправляются асинхронно в отдельном потоке, как только появляются "места").
    - В проект был добавлен lombok для улучшения читаемости кода.
    - Работа класса была полностью синхронизована.
    - Добавлен таймер для сброса значения счетчика, согласно заданному промежутку времени.

--- APIWorker/src/main/java/CrptApi.java
+++ APIWorker/src/main/java/CrptApi.java
@@ -1,7 +1,9 @@
 
 import com.google.gson.Gson;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Setter;
 import org.apache.http.HttpEntity;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -11,297 +13,268 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
+import javax.script.ScriptEngine;
 import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class CrptApi {
 
-    // Exceptions
-    protected static class InvalidRequestsLimitException extends Exception {
-        public InvalidRequestsLimitException(final int received, final int expected) {
-            super(
-                    String.format("Wrong requests count received: <%d>\nExpected: <%d>",
-                            received, expected)
-            );
-        }
+// Exceptions
+class InvalidRequestsLimitException extends Exception {
+    public InvalidRequestsLimitException(final int received, final int expected) {
+        super(
+                String.format("Wrong requests count received: <%d>\nExpected: <%d>",
+                        received, expected)
+        );
+    }
+}
+
+class TooMuchRequestsException extends Exception {
+    TooMuchRequestsException() {
+        super("The limit on the number of requests has been exceeded!");
+    }
+}
+
+
+// Data class
+@Data
+@AllArgsConstructor
+class Document { // Replace "YourClassName" with a suitable class name
+
+    private Description description;
+    private String doc_id;
+    private String doc_status;
+    private String doc_type;
+    private boolean importRequest;
+    private String owner_inn;
+    private String participant_inn;
+    private String producer_inn;
+    private String production_date;
+    private String production_type;
+    private List<Product> products;
+    private String reg_date;
+    private String reg_number;
+
+    // Nested classes
+    @Data
+    @AllArgsConstructor
+    public static class Description {
+        private String participantInn;
+
     }
 
-    // Data class
-    protected static class Document { // Replace "YourClassName" with a suitable class name
-
-        private Description description;
-        private String doc_id;
-        private String doc_status;
-        private String doc_type;
-        private boolean importRequest;
+    @Data
+    @AllArgsConstructor
+    public static class Product {
+        private String certificate_document;
+        private String certificate_document_date;
+        private String certificate_document_number;
         private String owner_inn;
-        private String participant_inn;
         private String producer_inn;
         private String production_date;
-        private String production_type;
-        private List<Product> products;
-        private String reg_date;
-        private String reg_number;
-
-        public Description getDescription() {
-            return description;
-        }
-
-        public void setDescription(Description description) {
-            this.description = description;
-        }
-
-        public String getDoc_id() {
-            return doc_id;
-        }
-
-        public void setDoc_id(String doc_id) {
-            this.doc_id = doc_id;
-        }
-
-        public String getDoc_status() {
-            return doc_status;
-        }
-
-        public void setDoc_status(String doc_status) {
-            this.doc_status = doc_status;
-        }
-
-        public String getDoc_type() {
-            return doc_type;
-        }
-
-        public void setDoc_type(String doc_type) {
-            this.doc_type = doc_type;
-        }
-
-        public boolean isImportRequest() {
-            return importRequest;
-        }
-
-        public void setImportRequest(boolean importRequest) {
-            this.importRequest = importRequest;
-        }
-
-        public String getOwner_inn() {
-            return owner_inn;
-        }
-
-        public void setOwner_inn(String owner_inn) {
-            this.owner_inn = owner_inn;
-        }
-
-        public String getParticipant_inn() {
-            return participant_inn;
-        }
-
-        public void setParticipant_inn(String participant_inn) {
-            this.participant_inn = participant_inn;
-        }
-
-        public String getProducer_inn() {
-            return producer_inn;
-        }
-
-        public void setProducer_inn(String producer_inn) {
-            this.producer_inn = producer_inn;
-        }
-
-        public String getProduction_date() {
-            return production_date;
-        }
-
-        public void setProduction_date(String production_date) {
-            this.production_date = production_date;
-        }
-
-        public String getProduction_type() {
-            return production_type;
-        }
-
-        public void setProduction_type(String production_type) {
-            this.production_type = production_type;
-        }
-
-        public List<Product> getProducts() {
-            return products;
-        }
-
-        public void setProducts(List<Product> products) {
-            this.products = products;
-        }
-
-        public String getReg_date() {
-            return reg_date;
-        }
-
-        public void setReg_date(String reg_date) {
-            this.reg_date = reg_date;
-        }
-
-        public String getReg_number() {
-            return reg_number;
-        }
-
-        public void setReg_number(String reg_number) {
-            this.reg_number = reg_number;
-        }
-
-        // Inner classes for nested structures
-
-        public static class Description {
-            private String participantInn;
-
-            public String getParticipantInn() {
-                return participantInn;
-            }
-
-            public void setParticipantInn(String participantInn) {
-                this.participantInn = participantInn;
-            }
-        }
-
-        public static class Product {
-            private String certificate_document;
-            private String certificate_document_date;
-            private String certificate_document_number;
-            private String owner_inn;
-            private String producer_inn;
-            private String production_date;
-            private String tnved_code;
-            private String uit_code;
-            private String uitu_code;
-
-            // Getters and setters for product fields
-        }
+        private String tnved_code;
+        private String uit_code;
+        private String uitu_code;
     }
+}
 
-    // Serializer interface
-    public interface ISerializer<T> {
-        String serializeObject(T obj);
 
-        T deserializeObject(String obj);
-    }
+// Serializer interface
+interface ISerializer<T> {
+    String serializeObject(T obj);
 
-    // Http requests sender interface
-    public interface IHttpSender{
-        String doGet(String url) throws IOException;
+    T deserializeObject(String obj);
+}
 
-        String doPost(String url, String jsonParams) throws IOException;
-    }
 
-    // Http requests sender default impl
-    protected static class DefaultHttpSender implements IHttpSender{
+// Http requests sender interface
+interface IHttpSender {
+    String doGet(String url) throws IOException;
 
-        @Override
-        public String doGet(String url) throws IOException {
-            try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-                HttpGet httpGet = new HttpGet(url);
-                // Выполнение запроса и получение ответа
-                return executeRequest(httpClient, httpGet);
-            }
-        }
+    String doPost(String url, String jsonParams) throws IOException;
+}
 
-        @Override
-        public String doPost(String url, String jsonParams) throws IOException {
-            try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-                HttpPost httpPost = new HttpPost(url);
 
-                // Установка заголовков (если необходимо)
-                httpPost.setHeader("Content-Type", "application/json");
+// Http requests sender default impl
+class DefaultHttpSender implements IHttpSender {
 
-                // Установка тела запроса
-                StringEntity requestEntity = new StringEntity(jsonParams);
-                httpPost.setEntity(requestEntity);
-
-                // Выполнение запроса и получение ответа
-                return executeRequest(httpClient, httpPost);
-            }
-        }
-
-        private String executeRequest(CloseableHttpClient httpClient, HttpRequestBase requestBase) throws IOException {
+    @Override
+    public String doGet(String url) throws IOException {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet httpGet = new HttpGet(url);
             // Выполнение запроса и получение ответа
-            try (CloseableHttpResponse response = httpClient.execute(requestBase)) {
-                // Проверка статуса ответа
-                if (response.getStatusLine().getStatusCode() == 200) {
-                    // Извлечение содержимого ответа
-                    HttpEntity entity = response.getEntity();
-                    if (entity != null) {
-                        return EntityUtils.toString(entity);
-                    }
-                } else {
-                    throw new RuntimeException("HTTP Request Failed with Status code: " +
-                            response.getStatusLine().getStatusCode());
+            return executeRequest(httpClient, httpGet);
+        }
+    }
+
+    @Override
+    public String doPost(String url, String jsonParams) throws IOException {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(url);
+
+            // Установка заголовков (если необходимо)
+            httpPost.setHeader("Content-Type", "application/json");
+
+            // Установка тела запроса
+            StringEntity requestEntity = new StringEntity(jsonParams);
+            httpPost.setEntity(requestEntity);
+
+            // Выполнение запроса и получение ответа
+            return executeRequest(httpClient, httpPost);
+        }
+    }
+
+    private String executeRequest(CloseableHttpClient httpClient, HttpRequestBase requestBase) throws IOException {
+        // Выполнение запроса и получение ответа
+        try (CloseableHttpResponse response = httpClient.execute(requestBase)) {
+            // Проверка статуса ответа
+            if (response.getStatusLine().getStatusCode() == 200) {
+                // Извлечение содержимого ответа
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    return EntityUtils.toString(entity);
                 }
+            } else {
+                throw new RuntimeException("HTTP Request Failed with Status code: " +
+                        response.getStatusLine().getStatusCode());
             }
-            return "";
         }
+        return "";
+    }
+}
+
+
+// Serializer default impl
+class DefaultJsonDocumentSerializer implements ISerializer<Document> {
+    private final Gson gson = new Gson();
+
+    @Override
+    public String serializeObject(Document obj) {
+        return gson.toJson(obj);
     }
 
-    // Serializer default impl
-    protected static class DefaultJsonDocumentSerializer implements ISerializer<Document> {
-        private final Gson gson = new Gson();
-
-        @Override
-        public String serializeObject(Document obj) {
-            return gson.toJson(obj);
-        }
-
-        @Override
-        public Document deserializeObject(String obj) {
-            return gson.fromJson(obj, Document.class);
-        }
+    @Override
+    public Document deserializeObject(String obj) {
+        return gson.fromJson(obj, Document.class);
     }
+}
 
-    // Fields
+
+public class CrptApi {
     private final int MIN_REQUESTS_LIMIT = 0;
-    private int requestsLimit;
-    private TimeUnit requestsCounterRefreshTimeUnit;
+    private final long TIMER_DELAY = 0;
+    private final long SINGLE_THREAD_SLEEP_INTERVAL = 1000;
+    private final String URL = "https://ismp.crpt.ru/api/v3/lk/documents/create";
+
+    private volatile int requestsLimit;
+    private volatile TimeUnit requestsCounterRefreshTimeUnit;
     private volatile int requestsCounter;
-    private volatile Long lastRequestTimeNanos;
+    private final Lock requestsCounterLock;
+    @Setter
     private ISerializer<Document> jsonSerializer;
     private IHttpSender httpSender;
-    private ExecutorService requestsQueueSingleThreadExecutor;
+    private final ExecutorService requestsQueueSingleThreadExecutor;
+    private final Timer requestsCounterRefreshTimer;
+
+    {
+        this.jsonSerializer = new DefaultJsonDocumentSerializer();
+        this.httpSender = new DefaultHttpSender();
+        requestsCounterLock = new ReentrantLock();
+        this.requestsCounterRefreshTimer = new Timer();
+        requestsCounterRefreshTimer.schedule(new RefreshTimerTask(),
+                TIMER_DELAY,
+                requestsCounterRefreshTimeUnit.toMillis(1));
+    }
 
     public CrptApi(final int requestsLimit, final TimeUnit requestsCounterRefreshTimeUnit) {
         this.requestsLimit = requestsLimit;
         this.requestsCounterRefreshTimeUnit = requestsCounterRefreshTimeUnit;
         this.requestsQueueSingleThreadExecutor = Executors.newSingleThreadExecutor();
-        this.jsonSerializer = new DefaultJsonDocumentSerializer();
-        this.httpSender = new DefaultHttpSender();
     }
 
-    public String create(Document document){
-    }
-
-    protected boolean checkLastRequestTimeAndSet(){
-        final long currentTime = System.nanoTime();
-        if (lastRequestTimeNanos != null ||
-                currentTime - lastRequestTimeNanos > requestsCounterRefreshTimeUnit.toNanos(1)){
-            lastRequestTimeNanos = currentTime;
-            return false;
+    public Future<String> create(Document document) {
+        try {
+            return CompletableFuture.completedFuture(sendRequest(document));
+        } catch (TooMuchRequestsException e) {
+            return requestsQueueSingleThreadExecutor.submit(new SingleThreadTask(document));
         }
-        lastRequestTimeNanos = currentTime;
-        return true;
     }
 
-    public void setRequestsCounterRefreshTimeUnit(TimeUnit requestsCounterRefreshTimeUnit) {
-        this.requestsCounterRefreshTimeUnit = requestsCounterRefreshTimeUnit;
+    protected String sendRequest(Document document) throws TooMuchRequestsException {
+        try {
+            requestsCounterLock.lock();
+            if (!checkCounter()) throw new TooMuchRequestsException();
+            String requestRes = httpSender.doPost(URL, jsonSerializer.serializeObject(document));
+            requestsCounter--;
+            return requestRes;
+        } catch (IOException e) {
+            System.out.println("Can not send your request!");
+            return "";
+        } finally {
+            requestsCounterLock.unlock();
+        }
     }
 
-    private void setRequestsLimit(final int requestsLimit) throws InvalidRequestsLimitException {
+    private boolean checkCounter() {
+        return requestsCounter > 0;
+    }
+
+    public void setRequestsLimit(final int requestsLimit) throws InvalidRequestsLimitException {
         if (requestsLimit < MIN_REQUESTS_LIMIT) {
             throw new InvalidRequestsLimitException(requestsLimit, MIN_REQUESTS_LIMIT);
         }
 
+        requestsCounterLock.lock();
         this.requestsLimit = requestsLimit;
+        requestsCounterLock.unlock();
     }
 
-    public void setJsonSerializer(ISerializer<Document> jsonSerializer) {
-        this.jsonSerializer = jsonSerializer;
+    public void setRequestsCounterRefreshTimeUnit(TimeUnit requestsCounterRefreshTimeUnit) {
+        this.requestsCounterRefreshTimeUnit = requestsCounterRefreshTimeUnit;
+        requestsCounterRefreshTimer.cancel();
+        requestsCounterRefreshTimer.schedule(new RefreshTimerTask(),
+                TIMER_DELAY,
+                requestsCounterRefreshTimeUnit.toMillis(1));
     }
 
+    // Nested classes
+    protected class RefreshTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            requestsCounterLock.lock();
+            requestsCounter = requestsLimit;
+            requestsCounterLock.unlock();
+        }
+    }
+
+    @AllArgsConstructor
+    protected class SingleThreadTask implements Callable<String> {
+        private Document documentToSend;
+
+        @Override
+        public String call() {
+            while (true) {
+                try {
+                    Thread.sleep(SINGLE_THREAD_SLEEP_INTERVAL);
+                    return sendRequest(documentToSend);
+                } catch (TooMuchRequestsException ignore) {
+                } catch (InterruptedException ex) {
+                    System.out.println("Single thread executor interrupted!");
+                }
+            }
+        }
+    }
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        CrptApi crptApi = new CrptApi(10, TimeUnit.MINUTES);
+        Document document = null; // Something document
+        crptApi.create(document);
+    }
 }
